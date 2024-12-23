1f262c654137d40adc4947f964396a39f0246a2d
shubn <shubn@MEATE>
2024-12-23 19:01:50 +0300

    model preparation

--- Data Science Project/Data_Science_Project.py
+++ Data Science Project/Data_Science_Project.py
@@ -2,12 +2,13 @@ import pandas as pd  # type: ignore
 from sklearn.preprocessing import LabelEncoder #type: ignore
 from sklearn.preprocessing import StandardScaler #type: ignore
 from sklearn.ensemble import RandomForestClassifier #type: ignore
-from sklearn.linear_model import LogisticRegression #type: ignore
+from sklearn.linear_model import LogisticRegression,LinearRegression #type: ignore
 from sklearn.pipeline import Pipeline #type: ignore
 from sklearn.compose import ColumnTransformer#type: ignore
 from sklearn.impute import SimpleImputer #type: ignore
 from sklearn.model_selection import train_test_split #type: ignore
-from sklearn.model_selection import KFold, cross_val_score #type: ignore
+from sklearn.model_selection import KFold, cross_val_score,cross_val_predict #type: ignore
+from sklearn.metrics import confusion_matrix #type: ignore
 
 def EncodeLabel(column,df):
     label_encoder = LabelEncoder()
@@ -29,7 +30,6 @@ df = df.drop(columns=["Movie Info","Genres","Critic Consensus","Authors","Actors
 
 #fill empty columns
 df["Directors"] = df["Directors"].fillna(df["Directors"].mode()[0])
-#df["Production Company"] = df["Production Company"].fillna("Unknown")
 df["Audience Rating"] = df["Audience Rating"].fillna(round(df["Audience Rating"].mean()))
 df["Audience Status"] = df["Audience Status"].fillna(df["Audience Status"].mode()[0])
 df["Audience Count"] = df["Audience Count"].ffill()
@@ -80,8 +80,6 @@ df['IMDB Votes'] = df['IMDB Votes'].str.replace(',', '').astype(int)
 #director labeling
 df = EncodeLabel("Directors",df)
 
-df.to_csv("new_oscars_data.csv", index = False)
-
 oscar_winner_count = df["Winner"].sum()
 
 print(oscar_winner_count)
@@ -89,18 +87,43 @@ print(oscar_winner_count)
 X = df.drop(columns = ["Winner"])
 y = df["Winner"]
 
-model = RandomForestClassifier(random_state=42)
 
+preprocessor = ColumnTransformer(transformers=[
+    ("num",StandardScaler(),["Movie Time","IMDB Rating","IMDB Votes","Tomatometer Rating","Tomatometer Count","Audience Rating","Audience Count",
+                            "Tomatometer Top Critics Count","Tomatometer Fresh Critics Count"]),
+    ("cat",SimpleImputer(strategy="most_frequent"),["Directors"])],remainder="passthrough")
+
+ 
+model_lr = Pipeline(steps=[
+    ("preprocessor", preprocessor),
+   ("classifier", LogisticRegression(solver="saga", max_iter=10000))
+])
+
+model_rf = Pipeline(steps=[
+    ("preprocessor",preprocessor),
+    ("classifier",RandomForestClassifier())])
+
+   
 kf = KFold(n_splits=5, shuffle=True, random_state=42)  
 
-print(X.dtypes)
-
 # Evaluate the model
-scores = cross_val_score(model, X, y, cv=kf, scoring='accuracy')  
+y_pred_rf = cross_val_predict(model_rf, X, y, cv=kf)
+
+lr_score = cross_val_score(model_rf,X,y,cv = kf, scoring="accuracy")
+
+# Check the shape of y_pred_lr and y
+print(lr_score)
+print(f"Shape of true labels: {y.shape}")
+print(f"Shape of predicted labels (Random Forest): {y_pred_rf.shape}")
+
+# Generate confusion matrix for Logistic Regression
+cm_rf = confusion_matrix(y, y_pred_rf)
+print("Confusion Matrix for Random Forest:")
+print(cm_rf)
 
 # Print Scores
-print("Cross-validation scores for each fold:", scores)
-print("Mean accuracy:", scores.mean())
+#print("Cross-validation scores for each fold:", y_pred_lr)
+#print("Mean accuracy:", y_pred_lr.mean())
 
 
 
